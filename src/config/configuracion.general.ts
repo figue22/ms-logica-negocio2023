8924cf0373ba889f6ec513bdf80b1ada73252598
Juan Manuel <juanmanuelfigueroavalencia@gmail.com>
2023-09-24 22:17:33 -0500

    Carga y descarga de archivos en lógica de negocio

---
+++ src/config/configuracion.general.ts
@@ -0,0 +1,6 @@
+export namespace ConfiguracionGeneral {
+  export const carpetaArchivosProductos: string = '../../archivos/productos';
+  export const campoDeProducto: string = "file";
+  export const extensionesImagenes: string[] = ['.JPG', '.PNG', '.JPEG'];
+  export const carpetaArchivosClientes: string = '../../archivos/clientes';
+}
